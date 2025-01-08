7ea52998454d5454e6750a0724181605081ce219
Manav Lal <manavlal5@gmail.com>
2025-01-08 15:52:15 +1300

    feat: spacing

--- src/page/thePage.tsx
+++ src/page/thePage.tsx
@@ -41,8 +41,10 @@ return (
            <Text fz="sm" className={classes.description} >
                At Sagar Indian Restaurant, we bring the authentic flavors of Indian cuisine to the North Shore. Our extensive menu offers something for every taste, including a variety of delicious vegetarian options.
            </Text>
-           <Button style={{marginRight:"2vw"}} onClick={scrollToFooter}>About Us</Button>
-           <Button onClick={scrollToMenu}>Menu</Button>
+           <Box style={{marginTop:"2vh"}}>
+           <Button style={{marginRight:"2vw"}} onClick={scrollToFooter} color={"primary.7"}>About Us</Button>
+           <Button onClick={scrollToMenu}  color={"primary.7"}>Menu</Button>
+           </Box>
            <Box>
                <Title style={{fontSize: "1.75rem", paddingTop: "2rem"}} size="h5" c="primary.9">
                    OPENING HOURS:
