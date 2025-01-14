c86402058fc68da8dec9566cccea403c5a6a4c89
Manav Lal <manavlal5@gmail.com>
2025-01-14 21:30:21 +1300

    feat: fixed margins on smalelr views and the price text wrapping

--- src/component/menuComponents/menuItem.tsx
+++ src/component/menuComponents/menuItem.tsx
@@ -11,11 +11,12 @@ const MenuItem: FC<ItemProps> = ({item}) => {
     return (
         <Box>
             <Box style={{display:"flex", justifyContent:"space-between"}}>
-                <Text tt="uppercase" size={"md"} c={"primary.8"}>{item.name}
+                <Text tt="uppercase" size={"sm"} c={"primary.8"} style={{margin:"0 1rem 0 0"}}>{item.name}
 
-                    {item.vegan ? ( <Tooltip label={"Can be made vegan"}><VeganIcon style={{color:"E03131", marginLeft: "1rem"}}/></Tooltip>):null}</Text>
-                
-                <Text tt="uppercase" size={"xs"} c={"primary.8"}>{item.price[0]} | {item.price[1]}</Text>
+                    {item.vegan ? ( <Tooltip label={"Can be made vegan"}><VeganIcon size={"2rem"} style={{color:"E03131", marginLeft: "1rem"}}/></Tooltip>):null}</Text>
+
+                <Text tt="uppercase" size={"xs"} c={"primary.8"} style={{whiteSpace: "nowrap"}}>{item.price[0]} | {item.price[1]}</Text>
+
             </Box>
             <Text size={"xs"}>{item.description}</Text>
         </Box>
