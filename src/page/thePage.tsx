b1b74418d78d9cf716b968ef5d4f56b3186edf91
Manav Lal <manavlal5@gmail.com>
2025-01-16 19:45:02 +1300

    fix: fixed phone screen scroll mabob + changed to SVG background DIV

--- src/page/thePage.tsx
+++ src/page/thePage.tsx
@@ -1,27 +1,27 @@
-import {Box, Title, Text, Button} from "@mantine/core";
+import {Box, Button, Text, Title} from "@mantine/core";
 import classes from "./thePage.module.css"
-import SagarLogo from '../assets/single.svg?react';
 import MenuComp from "../component/menuComponents/menu.tsx";
 import Footer from "../component/footer.tsx";
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
+
 const openingHours = [
-    { day: "MON - TUE", open: "5:00 PM", close: "9:00 PM" },
-    { day: "WED - SUN", open: "5:00 PM", close: "10:00 PM" },
+    {day: "MON - TUE", open: "5:00 PM", close: "9:00 PM"},
+    {day: "WED - SUN", open: "5:00 PM", close: "10:00 PM"},
 
 ];
 
-const ThePage = () =>{
+const ThePage = () => {
 
-    const scrollToFooter=()=>{
+    const scrollToFooter = () => {
         const element = document.getElementById("footer");
         if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
+            element.scrollIntoView({behavior: "smooth"});
         }
     }
-    const scrollToMenu=()=>{
+    const scrollToMenu = () => {
         const element = document.getElementById("menu");
         if (element) {
-            element.scrollIntoView({ behavior: "smooth" });
+            element.scrollIntoView({behavior: "smooth"});
         }
     }
 
@@ -38,57 +38,64 @@ const ThePage = () =>{
         window.addEventListener('resize', calculateLogos);
         return () => window.removeEventListener('resize', calculateLogos);
     }, []);
-return (
-    <Box className={classes.background}>
-        {/*<Box style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>*/}
-        {/*<SagarLogo></SagarLogo>*/}
-        {/*    <SagarLogo/>*/}
-        {/*    <SagarLogo/>*/}
-        {/*</Box>*/}
-            <Box
-                style={{
-                    display: 'flex',
-                    flexDirection:"row",
-                    justifyContent:"left",
-                    width:"100%"
-                }}
-            >
-                {Array.from({ length: logoCount }).map((_, index) => (
-                    <SagarLogo key={index} style={{ height: '100%' }} />
-                ))}
+    return (
+        <Box className={classes.background}>
+            <div className={classes.repeatingSvg}>
+
+            </div>
+            {/*<Box style={{display:"flex", flexDirection:"row", justifyContent:"center"}}>*/}
+            {/*<SagarLogo></SagarLogo>*/}
+            {/*    <SagarLogo/>*/}
+            {/*    <SagarLogo/>*/}
+            {/*</Box>*/}
+            {/*<Box*/}
+            {/*    style={{*/}
+            {/*        display: 'flex',*/}
+            {/*        flexDirection: "row",*/}
+            {/*        width: "100%"*/}
+            {/*    }}*/}
+            {/*>*/}
+
+            {/*    {Array.from({length: logoCount}).map((_, index) => (*/}
+            {/*        <span style={{marginLeft: "-1px"}}>*/}
+            {/*        <Single key={index}/>*/}
+            {/*        </span>*/}
+            {/*    ))}*/}
+            {/*</Box>*/}
+            <Box className={classes.container}>
+                <Title className={classes.mainTitle} size="h1" c="primary.9" fw={900}>
+                    SAGAR INDIAN <br/> RESTAURANT
+                </Title>
+                <Title size="h4" c="primary.9" fw={900}>
+                    73 MOKOIA ROAD, BIRKENHEAD, AUCKLAND
+                </Title>
+                <Title size={"h5"} c="primary.9" style={{fontFamily: "Noto-serif"}}>Ph: 09 4185019</Title>
+                <Text fz="sm" className={classes.description}>
+                    At Sagar Indian Restaurant, we bring the authentic flavors of Indian cuisine to the North Shore. Our
+                    extensive menu offers something for every taste, including a variety of delicious vegetarian
+                    options.
+                </Text>
+                <Box style={{marginTop: "4vh"}}>
+                    <Button style={{marginRight: "2vw"}} onClick={scrollToFooter} color={"primary.7"}>About Us</Button>
+                    <Button onClick={scrollToMenu} color={"primary.7"}>Menu</Button>
+                </Box>
+                <Box style={{marginTop: "4vh", marginBottom: "4vh"}}>
+                    <Title style={{fontSize: "1.75rem"}} size="h5" c="primary.9">
+                        OPENING HOURS:
+                    </Title>
+                    <ul style={{listStyleType: "none"}}>
+                        {openingHours.map(({day, open, close}) => (
+                            <li key={day} style={{display: "flex", justifyContent: "space-between", width: "40rem"}}>
+                                <Text size='xs' style={{margin: "0.25rem"}}>{day}</Text>
+                                <Text size='xs' style={{margin: "0.25rem"}}>{open} - {close}</Text>
+                            </li>
+                        ))}
+                    </ul>
+                </Box>
+                <MenuComp></MenuComp>
+            </Box>
+            <Footer/>
         </Box>
-       <Box className={classes.container}>
-           <Title  className={classes.mainTitle} size="h1" c="primary.9" fw={900}>
-               SAGAR INDIAN <br/> RESTAURANT
-           </Title>
-           <Title size="h4" c="primary.9" fw={900}>
-               73 MOKOIA ROAD, BIRKENHEAD, AUCKLAND
-           </Title>
-           <Title size={"h5"} c="primary.9" style={{fontFamily: "Noto-serif"}}>Ph: 09 4185019</Title>
-           <Text fz="sm" className={classes.description} >
-               At Sagar Indian Restaurant, we bring the authentic flavors of Indian cuisine to the North Shore. Our extensive menu offers something for every taste, including a variety of delicious vegetarian options.
-           </Text>
-           <Box style={{marginTop:"4vh"}}>
-           <Button style={{marginRight:"2vw"}} onClick={scrollToFooter} color={"primary.7"}>About Us</Button>
-           <Button onClick={scrollToMenu}  color={"primary.7"}>Menu</Button>
-           </Box>
-           <Box style={{marginTop:"4vh", marginBottom:"4vh"}}>
-               <Title style={{fontSize: "1.75rem"}} size="h5" c="primary.9">
-                   OPENING HOURS:
-               </Title>
-               <ul style={{ listStyleType: "none" }}>
-                   {openingHours.map(({day, open, close}) => (
-                       <li key={day} style={{display: "flex", justifyContent: "space-between", width:"40rem"}}>
-                           <Text size='xs' style={{margin:"0.25rem"}}>{day}</Text>
-                           <Text size='xs' style={{margin:"0.25rem"}}>{open} - {close}</Text>
-                       </li>
-                   ))}
-               </ul>
-           </Box>
-           <MenuComp></MenuComp>
-       </Box>
-    <Footer/>
-    </Box>
-)
+    )
 }
 export default ThePage;
